c0e86c8476ac7bc0cad3c031398d1fb625066d8d
fengzhengyi <fzktc@163.com>
2021-03-04 09:31:55 +0800

    fix

--- src/krouter/krouter-link.js
+++ src/krouter/krouter-link.js
@@ -1,5 +1,4 @@
 export default {
-<<<<<<< HEAD
     props: {
         to: {
             type: String,
@@ -9,15 +8,4 @@ export default {
     render (h) {
         return h('a', { attrs: { href: '#' + this.to } }, this.$slots.default)
     }
-=======
-  props: {
-    to: {
-      type: String,
-      require: true
-    }
-  },
-  render (h) {
-    return h('a', { attrs: { href: '#' + this.to } }, this.$slots.default)
-  }
->>>>>>> 4911d9d96e2526b9b0c0c142a294e7ee11ea0e19
 }
